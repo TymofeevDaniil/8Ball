e87a71adde437109e7577c63456ef684d4cc96ca
TymofeevDaniil <tymofeevdaniil@gmail.com>
2021-10-28 13:00:52 +0300

    mainVC view and Navigation
    button touch area issue

--- 8Ball/Main/MainViewController.swift
+++ 8Ball/Main/MainViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import SnapKit
+//import Foundation
 
 class MainViewController: UIViewController {
     
-    var b = UIButton()
+    var setButton = UIButton()
+    var answerLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,32 @@ class MainViewController: UIViewController {
     }
     
     func createUI() {
-        
+        view.addSubview(answerLabel)
+        answerLabel.font = .systemFont(ofSize: 30)
+        answerLabel.backgroundColor = .white
+        answerLabel.text = "Sample Text"
+        answerLabel.snp.makeConstraints{ (make) -> Void in
+            make.center.equalTo(view)
+        }
+
+        view.addSubview(setButton)
+        setButton.isUserInteractionEnabled = true
+        setButton.backgroundColor = .white
+        setButton.setImage(UIImage(systemName: "gear"), for: .normal)
+        setButton.setTitleColor(.black, for: .normal)
+        setButton.accessibilityActivationPoint = setButton.center
+        setButton.snp.makeConstraints{ (make) -> Void in
+//            make.centerY.equalTo(view.frame.minY + 70)
+//            make.centerX.equalTo(view.frame.maxX - 50)
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+            
+        }
+        setButton.addTarget(self, action: #selector(goToSettings(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func goToSettings(sender: UIButton!) {
+        show(SettingsViewController(), sender: self)
     }
 
 }
